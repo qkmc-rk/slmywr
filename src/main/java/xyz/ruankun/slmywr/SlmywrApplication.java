3a27ab877f1b026a4918c5ddbcf908126e934dda
mrruan <qkmc@outlook.com>
2021-07-12 16:12:00 +0800

    first commit

---
+++ src/main/java/xyz/ruankun/slmywr/SlmywrApplication.java
@@ -0,0 +1,13 @@
+package xyz.ruankun.slmywr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlmywrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SlmywrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/xyz/ruankun/slmywr/SlmywrApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.ruankun.slmywr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlmywrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
